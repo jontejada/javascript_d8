6594a7286ef59405c1a0d6dd46dc606239338db6
Jon Tejada <tejjon@gmail.com>
2015-11-03 17:25:08 -0800

    scope edit

--- scope_and_higher_order.js
+++ scope_and_higher_order.js
@@ -2,6 +2,7 @@
 notes from the Scopes, Hoisting, and Higher-Order Functions learning experience
 */
 
+//
 
 
 
@@ -37,11 +38,12 @@ console.log(closedOver); //closedOver is closed over / not defined in the global
 
 
 
+
 //callback functions -- any function that is passed in as a parameter to another function AND is called at the end of the function
 
 function higherOrder(aNumber,callBack) { //callBack is a parameter
     aNumber += 10;
-    callBack(aNumber); //and callBack is returned
+    callBack(aNumber); //and callBack is returned (invoking the callback)
 }
 
 var printHello = function() {
@@ -59,40 +61,6 @@ higherOrder(someNumber,printHello); //printHello is callback function, logs "Hel
 higherOrder(someNumber,printANumber); //printANumber is callback function, logs "printing a number" and "15"
 higherOrder(2,printANumber); //printANumber is callback function, logs "printing a number" and "12"
 
-
-
-/*
-function highOrderFunction(someNumber, callbackFunction) {
-    someNumber += 10;
-    callbackFunction(someNumber);
-}
-
-var printHello = function(){
-    console.log('Hello');
-};
-
-var printANumber = function(input) {
-    console.log("printing a number");
-    console.log(input);
-};
-
-var someNumber = 10;
-highOrderFunction(someNumber, printHello);
-highOrderFunction(someNumber, printANumber);
-
-
-
-This code is perfectly legal, and prints:
-Hello
-printing a number
-20
-In this example, printANumber and printHello both act as "callback functions".
-On the last two lines, we call highOrderFunction, 
-and each time the second parameter is a variable whose value is a function. 
-The last line of highOrderFunction invokes these "callback" functions.
-Callbacks are tricky, but incredibly common in JavaScript.
-Take a minute and create a couple of functions that use callbacks!
-*/
 
 
 
